45867427bef0b8b4df536c51a065fc771b3f94dc
MLops-34 <147312087+MLops34@users.noreply.github.com>
2025-10-23 19:25:05 +0530

    Create Baxics.py

---
+++ Python/Baxics.py
@@ -0,0 +1,58 @@
+a=10
+b = 5
+
+add_result=a+b  #addiiton
+sub_result=a-b  #substraction 
+mult_result=a*b #multiplication
+div_result=a/b  #division
+floor_div_result=a//b ## floor division
+modulus_result=a%b #modulus operation
+
+exponent_result=a**b ## Exponentiation
+
+
+print(add_result)
+print(sub_result)
+print(mult_result)
+print(div_result)
+print(floor_div_result)
+print(modulus_result)
+print(exponent_result)
+
+
+## Comparison Operators
+## == Equal to
+a=10
+b=10
+
+a==b
+
+
+## And ,Not,OR
+X=True
+Y=True
+
+result =X and Y
+print(result)
+
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Performing arithmetic operations
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+floor_division = num1 // num2
+modulus = num1 % num2
+exponentiation = num1 ** num2
+
+# Displaying results
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
+print("Floor Division:", floor_division)
+print("Modulus:", modulus)
+print("Exponentiation:", exponentiation)
